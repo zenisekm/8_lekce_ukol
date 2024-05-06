88380f413b05420ba9c106c4b435f1a1dfa7c4ef
zenisekm <zenisek96@gmail.com>
2024-05-06 20:40:53 +0200

    Změna lokace zdrojovych kodu.

--- simpleexample/src/main/java/cz/engeto/simpleexample/controller/HelloController.java
+++ simpleexample/src/main/java/cz/engeto/simpleexample/controller/HelloController.java
@@ -5,7 +5,6 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
